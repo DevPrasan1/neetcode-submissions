3bc581f9332dfa103a07a11d03bd72807ba5e387
Dev Prasan Bhardwaj <prasandev14@gmail.com>
2026-06-09 13:19:45 +0530

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let output = 0;
+        for(let left=0;left<s.length;left++){
+            let right = left;
+            const map={
+                [s[left]]: 1
+            };
+            let maxCharCount = 1;
+            let windowSize = right - left + 1;
+            while(right<s.length && (windowSize-maxCharCount)<=k){
+                right++;
+                const currChar = s[right];
+                map[currChar] =  (map[currChar]||0) + 1;
+                if(map[currChar]>maxCharCount){
+                    maxCharCount = map[currChar];
+                }
+                windowSize++;
+            }
+            output = Math.max(output, windowSize-1);
+            if(right===s.length){
+                break;
+            }
+        }
+        return output;
+    }
+}
