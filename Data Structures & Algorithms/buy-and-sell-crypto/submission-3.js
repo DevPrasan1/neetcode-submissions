1b01562dd3218d18cc4b74097ca073878a5309e5
Dev Prasan <prasandev14@gmail.com>
2026-09-16 23:47:01 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        if(prices.length<=1){
+            return 0;
+        }
+        let max = 0;
+        for(let i=0;i<prices.length-1;i++){
+            for(let j=i+1;j<prices.length;j++){
+                max = Math.max(max, prices[j]-prices[i])
+            }
+        }
+        return max;
+    }
+}
