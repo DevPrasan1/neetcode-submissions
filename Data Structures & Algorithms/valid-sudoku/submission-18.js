c4e12e56cebee1b652cedbc8f3923bc44b1423c8
Dev Prasan <prasandev14@gmail.com>
2026-09-25 11:23:49 +0530

    Add: valid-sudoku - submission-18

---
+++ Data Structures & Algorithms/valid-sudoku/submission-18.js
@@ -0,0 +1,76 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        // // validate rows
+
+        // function isDuplicate(item, section){
+        //     const num = +item;
+        //     if(num>0 && num<10){
+        //         if(section[num]){
+        //             return true;// duplicate number
+        //         }else{
+        //             section[num] = true;
+        //         }
+        //     }
+        //     return false;
+        // }
+        // const boxSize = 3;
+        // for(let r=0;r<9;r++){
+        //     let rowArray = [];
+        //     let colArray = [];
+        //     let boxArray = [];
+        //     let startX = Math.floor(r/boxSize)*boxSize;
+        //     let startY = r%boxSize*boxSize;
+        //     for(let c=0;c<9;c++){
+        //     if(c>0 && c%boxSize==0){
+        //         startX++;
+        //      }
+        //      const rowItem = board[r][c];
+        //      const colItem = board[c][r];
+        //      const boxY = startY + c%boxSize
+        //      const boxItem = board[startX][boxY];
+        //      console.log({ box : r, x: startX, y: boxY})
+        //      if(isDuplicate(rowItem, rowArray) || isDuplicate(colItem, colArray) || isDuplicate(boxItem, boxArray) ){
+        //         return false;
+        //      }
+        //     }
+        // }
+
+        const rowMap = {};
+        const colMap = {};
+        const sqMap = {};
+
+        function isDuplicate(map, key, item){
+            if(!map[key]){
+                map[key] = {};
+            }
+            if(map[key][item]){
+                return true;
+            }
+            map[key][item] = true;
+            return false;
+        }
+        for(let r=0;r<9;r++){
+            for(let c=0;c<9;c++){
+                const item = board[r][c];
+
+                const sqKey = 3*Math.floor(r/3) + Math.floor(c/3);
+                console.log({ r, c, sqKey });
+                if(item==".") continue;
+
+                if(
+                    isDuplicate(rowMap, r, item)
+                    || isDuplicate(colMap, c, item)
+                    || isDuplicate(sqMap, sqKey, item)
+                    ){
+                        return false;
+                    }
+            
+            }
+        }
+        return true;
+    }
+}
